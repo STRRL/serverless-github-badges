f2ecfcba5ed8c2e69e78444cc17598135ebe2f81
STRRL <im@strrl.dev>
2022-07-20 22:37:07 +0800

    chore: disable throttling
    Signed-off-by: STRRL <im@strrl.dev>

--- src/github.ts
+++ src/github.ts
@@ -1,15 +1,19 @@
 import { App, Octokit } from "octokit";
 import { createAppAuth } from "@octokit/auth-app";
+import { throttling } from "@octokit/plugin-throttling";
 import { Span } from "@cloudflare/workers-honeycomb-logger";
 import Toucan from "toucan-js";
 export default class GitHubClient {
-  octokit: Octokit
+  octokit: Octokit;
 
-  constructor(appId: string,
+  constructor(
+    appId: string,
     privateKey: string,
     installationID: number,
-    tracer: Span) {
-    this.octokit = new Octokit({
+    tracer: Span
+  ) {
+    const OctokitWithThrottling = Octokit.plugin(throttling);
+    this.octokit = new OctokitWithThrottling({
       authStrategy: createAppAuth,
       auth: {
         appId: appId,
@@ -17,25 +21,25 @@ export default class GitHubClient {
         installationId: installationID,
       },
       request: {
-        fetch: tracer.fetch.bind(tracer)
-      }
+        fetch: tracer.fetch.bind(tracer),
+      },
+      throttle: {
+        enabled: false,
+      },
     });
   }
-  async countOfPublicGistsOfUser(
-    user: string,
-  ) {
-   
-    const userGetResponse = await this.octokit.rest.users.getByUsername(
-      { username: user }
-    )
-    return userGetResponse.data.public_gists
+  async countOfPublicGistsOfUser(user: string) {
+    const userGetResponse = await this.octokit.rest.users.getByUsername({
+      username: user,
+    });
+    return userGetResponse.data.public_gists;
   }
 
   async githubRepoExisted(
     owner: string,
     repo: string,
-    installationID:string,
-    sentry:Toucan,
+    installationID: string,
+    sentry: Toucan
   ): Promise<boolean> {
     try {
       const repoGetResponse = await this.octokit.rest.repos.get({
@@ -52,9 +56,7 @@ export default class GitHubClient {
     }
   }
 
-  async howLongGithubUserCreatedInYears(
-    user: string,
-  ): Promise<number> {
+  async howLongGithubUserCreatedInYears(user: string): Promise<number> {
     const userGetResponse = await this.octokit.rest.users.getByUsername({
       username: user,
     });
@@ -68,66 +70,60 @@ export default class GitHubClient {
     user: string,
     appId: string,
     privateKey: string,
-    installationID: number,
+    installationID: number
   ) {
-    const userGetResponse = await this.octokit.rest.users.getByUsername(
-      { username: user }
-    )
-    return userGetResponse.data.public_repos
+    const userGetResponse = await this.octokit.rest.users.getByUsername({
+      username: user,
+    });
+    return userGetResponse.data.public_repos;
   }
 
-  async timeOfRepositoryLastUpdated(
-    owner: string,
-    repository: string,
-  ) {
+  async timeOfRepositoryLastUpdated(owner: string, repository: string) {
     const getRepoResponse = await this.octokit.rest.repos.get({
       owner: owner,
       repo: repository,
-    })
-    return getRepoResponse.data.updated_at
+    });
+    return getRepoResponse.data.updated_at;
   }
 
-
-  async timeOfRepositoryCreated(
-    owner: string,
-    repository: string,
-  ) {
-
+  async timeOfRepositoryCreated(owner: string, repository: string) {
     const getRepoResponse = await this.octokit.rest.repos.get({
       owner: owner,
       repo: repository,
-    })
-    return getRepoResponse.data.created_at
+    });
+    return getRepoResponse.data.created_at;
   }
-  async countOfCommitsAfterDate(
-    user: string,
-    start: Date,
-  ) {
-    const q = `author:${user}+author-date:>=${start.toISOString().split('T')[0]}`
-    const searchCommitsResponse = await this.octokit.rest.search.commits({ q: q })
+  async countOfCommitsAfterDate(user: string, start: Date) {
+    const q = `author:${user}+author-date:>=${
+      start.toISOString().split("T")[0]
+    }`;
+    const searchCommitsResponse = await this.octokit.rest.search.commits({
+      q: q,
+    });
     return searchCommitsResponse.data.total_count;
   }
 
   async countOfIssueOrPRsAfterDate(
     user: string,
     start: Date,
-    searchTarget: 'issue' | 'pr' | 'both',
+    searchTarget: "issue" | "pr" | "both"
   ) {
-    let q = `author:${user}+created:>=${start.toISOString().split('T')[0]}`
+    let q = `author:${user}+created:>=${start.toISOString().split("T")[0]}`;
     switch (searchTarget) {
       case "issue":
-        q += "+type:issue"
+        q += "+type:issue";
         break;
       case "pr":
-        q += '+is:pr'
+        q += "+is:pr";
         break;
       case "both":
         break;
       default:
-        throw new Error(`unrecognized searchTarget ${searchTarget}`)
+        throw new Error(`unrecognized searchTarget ${searchTarget}`);
     }
 
-    const searchIssuesAndPRsResponse = await this.octokit.rest.search.issuesAndPullRequests({ q: q })
+    const searchIssuesAndPRsResponse =
+      await this.octokit.rest.search.issuesAndPullRequests({ q: q });
     return searchIssuesAndPRsResponse.data.total_count;
   }
 
@@ -135,11 +131,10 @@ export default class GitHubClient {
     user: string,
     start: Date,
     end: Date,
-    allContribution: boolean,
+    allContribution: boolean
   ): Promise<number> {
-
     if (allContribution) {
-      const response = await this.octokit.graphql(
+      const response = (await this.octokit.graphql(
         `
   query { 
     user(login: "${user}") {
@@ -150,15 +145,23 @@ export default class GitHubClient {
     }
   }
           `
-      ) as any
-      const years = response.user.contributionsCollection.contributionYears as Array<number>
-      const results = await Promise.all(years.map(async year => {
-        const yearStart = new Date(`${year}-01-01`)
-        const yearEnd = new Date(`${year}-12-31`)
-        return await this.countOfUserContributionsAfterDate(user, yearStart, yearEnd, false)
-      }))
-      const allContributions = results.reduce((prev, curr) => prev + curr, 0)
-      return allContributions
+      )) as any;
+      const years = response.user.contributionsCollection
+        .contributionYears as Array<number>;
+      const results = await Promise.all(
+        years.map(async (year) => {
+          const yearStart = new Date(`${year}-01-01`);
+          const yearEnd = new Date(`${year}-12-31`);
+          return await this.countOfUserContributionsAfterDate(
+            user,
+            yearStart,
+            yearEnd,
+            false
+          );
+        })
+      );
+      const allContributions = results.reduce((prev, curr) => prev + curr, 0);
+      return allContributions;
     }
 
     const response = await this.octokit.graphql(
@@ -176,9 +179,10 @@ export default class GitHubClient {
     }
   }
     `
-    )
+    );
 
-    const result = (response as any).user.contributionsCollection.contributionCalendar.totalContributions as number
+    const result = (response as any).user.contributionsCollection
+      .contributionCalendar.totalContributions as number;
     return result;
   }
 }
